703a8dbaf93ef2a59fc212a12c9b61808a7145f6
Jorgeegp <100363649@alumnos.uc3m.es>
2019-04-03 00:12:45 +0200

    MicroP07
    Micro práctica 7

---
+++ microp_4/Agenda/src/app/firebase.credentials.ts
@@ -0,0 +1,10 @@
+
+  // Inicializar Firebase
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCjsHY4Al6w3JPGdIeUvETgex6RShjDYec",
+    authDomain: "micropractica7.firebaseapp.com",
+    databaseURL: "https://micropractica7.firebaseio.com",
+    projectId: "micropractica7",
+    storageBucket: "",
+    messagingSenderId: "538280357519"
+  };  
